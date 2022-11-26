c05c5e2c5853cffca87029d231a17de871373c1d
Dimitar <wopz1337@gmail.com>
2022-11-26 12:10:18 +0200

    Added Router With Routes In App.jsx Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
 
-const App = () => {
-  return <div>App</div>;
-};
+import "./App.css";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+const App = () => (
+  <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Router>
+  </Box>
+);
 
 export default App;
