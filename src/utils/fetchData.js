52d99517bce841e7484332c080a6443ba346644a
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-05-28 14:36:01 +0300

    Fix Error With API KEY

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
   method: "GET",
   headers: {
     "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-    "X-RapidAPI-Key": "02ebdf8f39msh849422b58c8f778p1c93ccjsnf9abaf901bc1",
+    "X-RapidAPI-Key": "46d1ecdc06msh26e8445fbb4b4a0p16bac7jsn6c33421a5983",
   },
 };
 
